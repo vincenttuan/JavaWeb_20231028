1144542e701e3c2d3019c58f86c8fc4e2762a79a
MB-teacher <MB-teacher@MB-207-teacher>
2023-11-04 10:34:30 +0800

    java

--- JavaWeb_20231028/src/main/java/controller/ScoreServlet.java
+++ JavaWeb_20231028/src/main/java/controller/ScoreServlet.java
@@ -15,7 +15,7 @@ public class ScoreServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 取得多筆 score 參數資料
-		
+		resp.getWriter().print("ScoreServlet");
 	}
 	
 
