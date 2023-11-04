0122c6685ba1d7719c0ed4f90b6d9d9a98ff8b32
MB-teacher <MB-teacher@MB-207-teacher>
2023-11-04 10:43:35 +0800

    java

--- JavaWeb_20231028/src/main/java/controller/ScoreServlet.java
+++ JavaWeb_20231028/src/main/java/controller/ScoreServlet.java
@@ -2,6 +2,7 @@ package controller;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.IntSummaryStatistics;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -17,8 +18,15 @@ public class ScoreServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 取得多筆 score 參數資料
 		String[] scores = req.getParameterValues("score");
-		resp.getWriter().print(Arrays.toString(scores));
+		resp.getWriter().println(Arrays.toString(scores));
 		
+		// 計算 scores 的總分, 平均, 個數, 最高分, 最低分
+		IntSummaryStatistics stat = Arrays.stream(scores).mapToInt(Integer::parseInt).summaryStatistics();
+		resp.getWriter().println("count: " + stat.getCount());
+		resp.getWriter().println("sum: " + stat.getSum());
+		resp.getWriter().println("avg: " + stat.getAverage());
+		resp.getWriter().println("max: " + stat.getMax());
+		resp.getWriter().println("min: " + stat.getMin());
 	}
 	
 
