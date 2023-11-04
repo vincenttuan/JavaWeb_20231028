59dd490d361e8772b590dfec1f29d7b051a708a9
MB-teacher <MB-teacher@MB-207-teacher>
2023-11-04 11:06:22 +0800

    java

--- JavaWeb_20231028/src/main/java/model/ScoreService.java
+++ JavaWeb_20231028/src/main/java/model/ScoreService.java
@@ -8,7 +8,9 @@ import java.util.Map;
 public class ScoreService {
 	
 	public Map<String, Number> getScoreMap(String[] scores) {
-		IntSummaryStatistics stat = Arrays.stream(scores).mapToInt(Integer::parseInt).summaryStatistics();
+		IntSummaryStatistics stat = Arrays.stream(scores)
+										  .mapToInt(Integer::parseInt)
+										  .summaryStatistics();
 		// 將 總分, 平均, 個數, 最高分, 最低分 放到一個 Map 集合中
 		Map<String, Number> scoreMap = new LinkedHashMap<>();
 		scoreMap.put("count", stat.getCount());
