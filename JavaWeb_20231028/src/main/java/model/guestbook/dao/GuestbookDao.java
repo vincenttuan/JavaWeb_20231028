9de8554ba92dff989c8897295682d73aafa94bd6
MB-teacher <MB-teacher@MB-207-teacher>
2023-11-11 10:55:24 +0800

    java

---
+++ JavaWeb_20231028/src/main/java/model/guestbook/dao/GuestbookDao.java
@@ -0,0 +1,26 @@
+package model.guestbook.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+// 與資料庫進行 CRUD
+// 利用 SingleTon 設計模式
+public class GuestbookDao {
+	
+	private Connection conn;
+	
+	private GuestbookDao() {
+		String dbUrl = "jdbc:mysql://localhost:3306/demo";
+		String username = "root";
+		String password = "12345678";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(dbUrl, username, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+}
