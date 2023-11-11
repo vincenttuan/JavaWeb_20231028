000d68df5c229c7caaf3c6a9e74c5c39ca99cb7d
MB-teacher <MB-teacher@MB-207-teacher>
2023-11-11 13:49:32 +0800

    java

---
+++ JavaWeb_20231028/src/main/java/model/guestbook/entity/PageInfo.java
@@ -0,0 +1,29 @@
+package model.guestbook.entity;
+
+public class PageInfo {
+	private Integer recordsOfPage;
+	private Integer count;
+	private Integer maxPage;
+	
+	public Integer getRecordsOfPage() {
+		return recordsOfPage;
+	}
+	public void setRecordsOfPage(Integer recordsOfPage) {
+		this.recordsOfPage = recordsOfPage;
+	}
+	public Integer getCount() {
+		return count;
+	}
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+	public Integer getMaxPage() {
+		return maxPage;
+	}
+	public void setMaxPage(Integer maxPage) {
+		this.maxPage = maxPage;
+	}
+	
+	
+	
+}
