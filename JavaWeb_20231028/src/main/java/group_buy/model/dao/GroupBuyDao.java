318103f8e3eea1e6acf616fb2e336662d7bbb35f
MB-teacher <MB-teacher@MB-207-teacher>
2023-11-18 15:09:48 +0800

    java

--- JavaWeb_20231028/src/main/java/group_buy/model/dao/GroupBuyDao.java
+++ JavaWeb_20231028/src/main/java/group_buy/model/dao/GroupBuyDao.java
@@ -2,6 +2,7 @@ package group_buy.model.dao;
 
 import java.util.List;
 
+import group_buy.model.entity.Cart;
 import group_buy.model.entity.Product;
 import group_buy.model.entity.User;
 
@@ -15,6 +16,16 @@ public interface GroupBuyDao {
 	Product getProductById(Integer id); // 取得商品
 	void addProduct(Product product); // 新增商品
 	void setLaunchProduct(Integer productId, Boolean isLaunch); // 調整商品上下架
+	
+	List<Cart> queryAllCarts(); // 取得所有購物車主檔
+	List<Cart> queryAllCartsByUserId(Integer id); // 取得該使用者的所有購物車主檔
+	List<Cart> queryAllCartsByUserId(Integer id, Boolean isCheckedOut); // 取得該使用者的所有購物車主檔(是否有結帳紀錄)
+	Cart getNoneCheckoutCartByUserId(Integer id); // 取得該使用者尚未結帳的購物車資料
+	
+	void addCart(Cart cart); // 新增購物車
+	void setCheckOutCartByUserId(Integer userId); // 將該使用者的購物車資訊進行結帳  
+	void setCheckOutCartByChartId(Integer cartId); // 將該購物車資訊進行結帳  
+	void deleteCartItemById(Integer cartItemId); // 將該筆購物明細刪除
 	
 	
 }
