b05e64bcfdd83be935922e5c6a1f8f9beba8ace7
MB-teacher <MB-teacher@MB-207-teacher>
2023-11-18 14:42:49 +0800

    java

--- JavaWeb_20231028/src/main/java/group_buy/model/entity/Cart.java
+++ JavaWeb_20231028/src/main/java/group_buy/model/entity/Cart.java
@@ -7,15 +7,17 @@ public class Cart {
 	private Integer id; // 序號
 	private Integer userId; // 使用者 id
 	private List<CartItem> cartItems;
+	private Boolean isCheckedOut; // 是否已經結帳
 	
 	public Cart() {
 		
 	}
-	
-	public Cart(Integer id, Integer userId, List<CartItem> cartItems) {
+
+	public Cart(Integer id, Integer userId, List<CartItem> cartItems, Boolean isCheckedOut) {
 		this.id = id;
 		this.userId = userId;
 		this.cartItems = cartItems;
+		this.isCheckedOut = isCheckedOut;
 	}
 
 	public Integer getId() {
@@ -42,10 +44,20 @@ public class Cart {
 		this.cartItems = cartItems;
 	}
 
+	public Boolean getIsCheckedOut() {
+		return isCheckedOut;
+	}
+
+	public void setIsCheckedOut(Boolean isCheckedOut) {
+		this.isCheckedOut = isCheckedOut;
+	}
+
 	@Override
 	public String toString() {
-		return "Cart [id=" + id + ", userId=" + userId + ", cartItems=" + cartItems + "]";
+		return "Cart [id=" + id + ", userId=" + userId + ", cartItems=" + cartItems + ", isCheckedOut=" + isCheckedOut
+				+ "]";
 	}
+	
 	
 	
 }
