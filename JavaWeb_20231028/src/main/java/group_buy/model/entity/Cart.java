7fee9a744d967d93ef05e8925481ebabc849ae5a
MB-teacher <MB-teacher@MB-207-teacher>
2023-11-18 14:29:15 +0800

    java

---
+++ JavaWeb_20231028/src/main/java/group_buy/model/entity/Cart.java
@@ -0,0 +1,7 @@
+package group_buy.model.entity;
+
+// 購物車
+public class Cart {
+	private Integer id; // 序號
+	private 
+}
--- JavaWeb_20231028/src/main/java/group_buy/model/entity/User.java
+++ JavaWeb_20231028/src/main/java/group_buy/model/entity/User.java
@@ -2,6 +2,7 @@ package group_buy.model.entity;
 
 // 使用者
 public class User {
+	private Integer id; // id
 	private String username;
 	private String password;
 	private Integer level; // 0: 訪客, 1: 後台人員
@@ -9,37 +10,51 @@ public class User {
 	public User() {
 		
 	}
-	
-	public User(String username, String password, Integer level) {
+
+	public User(Integer id, String username, String password, Integer level) {
+		super();
+		this.id = id;
 		this.username = username;
 		this.password = password;
 		this.level = level;
 	}
-	
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public Integer getLevel() {
 		return level;
 	}
+
 	public void setLevel(Integer level) {
 		this.level = level;
 	}
 
 	@Override
 	public String toString() {
-		return "User [username=" + username + ", password=" + password + ", level=" + level + "]";
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", level=" + level + "]";
 	}
-	
 	
 	
 }
