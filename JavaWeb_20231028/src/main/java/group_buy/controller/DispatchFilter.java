995337433f230dd5aebfef4a220b3ed3fdf3b676
MB-teacher <MB-teacher@MB-207-teacher>
2023-11-25 15:10:40 +0800

    java

--- JavaWeb_20231028/src/main/java/group_buy/controller/DispatchFilter.java
+++ JavaWeb_20231028/src/main/java/group_buy/controller/DispatchFilter.java
@@ -15,6 +15,9 @@ import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import static group_buy.controller.URLPath.團購首頁;
+import static group_buy.controller.URLPath.登入首頁;;
+
 // 過濾路徑分派器
 @WebFilter(value = {"/group_buy/*"})
 public class DispatchFilter extends HttpFilter {
@@ -31,7 +34,7 @@ public class DispatchFilter extends HttpFilter {
 		System.out.println(servletPath + ", " + method);
 		
 		switch (servletPath) {
-			case "/group_buy/frontend/main.jsp": // 按下登入頁的"前台登入"按鈕或按下"團購首頁"連結
+			case 團購首頁: // 按下登入頁的"前台登入"按鈕或按下"團購首頁"連結
 				if(method.equals("POST")) { // 按下登入頁的"前台登入"按鈕
 					String username = request.getParameter("username");
 					String password = request.getParameter("password");
@@ -40,7 +43,7 @@ public class DispatchFilter extends HttpFilter {
 					// 判斷是否有找到使用者與密碼判斷
 					if(userOpt.isEmpty() || !password.equals(userOpt.get().getPassword())) {
 						System.out.println("無此使用者或密碼不正確");
-						response.sendRedirect(contextPath + "/group_buy/login.jsp");
+						response.sendRedirect(contextPath + 登入首頁);
 						return;
 					}
 					System.out.println("使用者登入成功");
---
+++ JavaWeb_20231028/src/main/java/group_buy/controller/URLPath.java
@@ -0,0 +1,7 @@
+package group_buy.controller;
+
+public interface URLPath {
+	String 登入首頁 = "/group_buy/login.jsp";
+	String 團購首頁 = "/group_buy/frontend/main.jsp";
+	
+}
