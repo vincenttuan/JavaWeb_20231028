87139d55ef20c0544e74ee8680b9b58e688555a5
MB-teacher <MB-teacher@MB-207-teacher>
2023-11-25 15:38:38 +0800

    java

--- JavaWeb_20231028/src/main/java/group_buy/controller/URLPath.java
+++ JavaWeb_20231028/src/main/java/group_buy/controller/URLPath.java
@@ -3,5 +3,5 @@ package group_buy.controller;
 public interface URLPath {
 	String 登入首頁 = "/group_buy/login.jsp";
 	String 團購首頁 = "/group_buy/frontend/main.jsp";
-	
+	String 新增完成頁 = "/group_buy/frontend/result.jsp";
 }
