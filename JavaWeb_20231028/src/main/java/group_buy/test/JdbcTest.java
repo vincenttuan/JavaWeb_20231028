050e91a81f95031ea095cec3d94844511b719e00
MB-teacher <MB-teacher@MB-207-teacher>
2023-11-25 13:37:51 +0800

    java

--- JavaWeb_20231028/src/main/java/group_buy/test/JdbcTest.java
+++ JavaWeb_20231028/src/main/java/group_buy/test/JdbcTest.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import group_buy.model.dao.GroupBuyDao;
 import group_buy.model.dao.GroupBuyDaoMySQL;
+import group_buy.model.entity.Cart;
 import group_buy.model.entity.CartItem;
 import group_buy.model.entity.User;
 
@@ -15,6 +16,7 @@ public class JdbcTest {
 		List<User> users = dao.findAllUsers();
 		System.out.println(users.size());
 		System.out.println(users);
+		System.out.println();
 		
 		Optional<CartItem> cartItemOpt = dao.findCartItemById(1);
 		if(cartItemOpt.isPresent()) {
@@ -22,6 +24,15 @@ public class JdbcTest {
 			System.out.println(cartItem);
 		} else {
 			System.out.println("無此 cartitem 資料");
+		}
+		System.out.println();
+		
+		Optional<Cart> cartOpt = dao.findCartById(201);
+		if(cartOpt.isPresent()) {
+			Cart cart = cartOpt.get();
+			System.out.println(cart);
+		} else {
+			System.out.println("無此 cart 資料");
 		}
 		
 	}
