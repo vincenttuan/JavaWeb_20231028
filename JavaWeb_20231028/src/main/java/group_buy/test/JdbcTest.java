289a4575cfe7be6572354f2f59ab2f4958fe5c24
MB-teacher <MB-teacher@MB-207-teacher>
2023-11-25 12:13:01 +0800

    java

--- JavaWeb_20231028/src/main/java/group_buy/test/JdbcTest.java
+++ JavaWeb_20231028/src/main/java/group_buy/test/JdbcTest.java
@@ -1,9 +1,11 @@
 package group_buy.test;
 
 import java.util.List;
+import java.util.Optional;
 
 import group_buy.model.dao.GroupBuyDao;
 import group_buy.model.dao.GroupBuyDaoMySQL;
+import group_buy.model.entity.CartItem;
 import group_buy.model.entity.User;
 
 public class JdbcTest {
@@ -13,6 +15,15 @@ public class JdbcTest {
 		List<User> users = dao.findAllUsers();
 		System.out.println(users.size());
 		System.out.println(users);
+		
+		Optional<CartItem> cartItemOpt = dao.findCartItemById(1);
+		if(cartItemOpt.isPresent()) {
+			CartItem cartItem = cartItemOpt.get();
+			System.out.println(cartItem);
+		} else {
+			System.out.println("無此 cartitem 資料");
+		}
+		
 	}
 
 }
