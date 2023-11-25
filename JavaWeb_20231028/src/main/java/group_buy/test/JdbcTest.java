4dddc54ba32134328a048a61c1c4d51bd243735f
MB-teacher <MB-teacher@MB-207-teacher>
2023-11-25 10:55:56 +0800

    java

--- JavaWeb_20231028/src/main/java/group_buy/test/JdbcTest.java
+++ JavaWeb_20231028/src/main/java/group_buy/test/JdbcTest.java
@@ -1,10 +1,18 @@
 package group_buy.test;
 
+import java.util.List;
+
+import group_buy.model.dao.GroupBuyDao;
+import group_buy.model.dao.GroupBuyDaoMySQL;
+import group_buy.model.entity.User;
+
 public class JdbcTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GroupBuyDao dao = GroupBuyDaoMySQL.getInstance();
+		List<User> users = dao.findAllUsers();
+		System.out.println(users.size());
+		System.out.println(users);
 	}
 
 }
