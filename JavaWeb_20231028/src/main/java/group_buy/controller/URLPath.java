23f50b34982cba0a639c5884cd70052c8a585d3d
MB-teacher <MB-teacher@MB-207-teacher>
2023-12-02 13:43:53 +0800

    java

--- JavaWeb_20231028/src/main/java/group_buy/controller/URLPath.java
+++ JavaWeb_20231028/src/main/java/group_buy/controller/URLPath.java
@@ -4,6 +4,9 @@ public interface URLPath {
 	String 登入首頁 = "/group_buy/login.jsp";
 	String 團購首頁 = "/group_buy/frontend/main.jsp";
 	String 新增完成頁 = "/group_buy/frontend/result.jsp";
-	String 購物車頁 = "/group_buy/frontend/cart.jsp";
+	String 購物車頁 = "/group_buy/frontend/cart.jsp"; // 購物出項目刪除與修改
 	String 結帳成功 = "/group_buy/frontend/finish.jsp";
+	
+	String 後台商品新增 = "/group_buy/backend/result.jsp";
+	
 }
