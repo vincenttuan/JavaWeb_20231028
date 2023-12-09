bb4672494b1dc38153e0356d52f297039522ab79
MB-teacher <MB-teacher@MB-207-teacher>
2023-12-09 11:44:41 +0800

    java

--- JavaWeb_20231028/src/main/java/rest/booking/entity/BookingRoom.java
+++ JavaWeb_20231028/src/main/java/rest/booking/entity/BookingRoom.java
@@ -1,5 +1,49 @@
 package rest.booking.entity;
 
-public class BookingRoom {
+import java.sql.Timestamp;
 
+public class BookingRoom {
+	private Integer bookingId;
+	private Integer roomId;
+	private String username;
+	private String bookingDate;
+	private Timestamp createTime;
+	
+	public Integer getBookingId() {
+		return bookingId;
+	}
+	public void setBookingId(Integer bookingId) {
+		this.bookingId = bookingId;
+	}
+	public Integer getRoomId() {
+		return roomId;
+	}
+	public void setRoomId(Integer roomId) {
+		this.roomId = roomId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getBookingDate() {
+		return bookingDate;
+	}
+	public void setBookingDate(String bookingDate) {
+		this.bookingDate = bookingDate;
+	}
+	public Timestamp getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Timestamp createTime) {
+		this.createTime = createTime;
+	}
+	@Override
+	public String toString() {
+		return "BookingRoom [bookingId=" + bookingId + ", roomId=" + roomId + ", username=" + username
+				+ ", bookingDate=" + bookingDate + ", createTime=" + createTime + "]";
+	}
+	
+	
 }
--- JavaWeb_20231028/src/main/java/rest/booking/entity/Room.java
+++ JavaWeb_20231028/src/main/java/rest/booking/entity/Room.java
@@ -1,5 +1,25 @@
 package rest.booking.entity;
 
 public class Room {
-
+	private Integer roomId;
+	private String roomName;
+	
+	public Integer getRoomId() {
+		return roomId;
+	}
+	public void setRoomId(Integer roomId) {
+		this.roomId = roomId;
+	}
+	public String getRoomName() {
+		return roomName;
+	}
+	public void setRoomName(String roomName) {
+		this.roomName = roomName;
+	}
+	
+	@Override
+	public String toString() {
+		return "Room [roomId=" + roomId + ", roomName=" + roomName + "]";
+	}
+	
 }
