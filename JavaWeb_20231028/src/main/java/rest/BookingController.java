34fc7db398bb7a82b8c11dcf501048e7e815e3e4
MB-teacher <MB-teacher@MB-207-teacher>
2023-12-09 10:20:17 +0800

    java

--- JavaWeb_20231028/src/main/java/rest/BookingController.java
+++ JavaWeb_20231028/src/main/java/rest/BookingController.java
@@ -7,10 +7,14 @@ import jakarta.servlet.http.HttpServlet;
  * 會議室預訂系統(規定使用 REST/RESTful)
  * 假設您正在為一家公司開發一個會議室預訂系統。
  * 您需要實現一個控制器，該控制器可以處理會議室的
- * 1. 預訂請求
- * 2. 取消請求
+ * 1. 新增預約
+ * 	  POST /rest/booking/	
+ * 2. 取消預約
+ *    DELETE /rest/booking/1
  * 3. 變更預約時間
- * 4. 查詢當前預訂狀態。
+ *    PUT /rest/booking/1
+ * 4. 查詢當前所有預約狀態。
+ * 	  GET /rest/booking/ 
  */
 @WebServlet("/rest/booking/*")
 public class BookingController extends HttpServlet {
